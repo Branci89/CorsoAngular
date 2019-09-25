4062dceaa60d83ee94c855af435d37cdac5ac8c5
Branci89 <cbranci89@gmail.com>
2019-09-25 16:55:08 +0200

    inizio tentativo di form per inserire un oggetto

--- AppGG2/src/app/app-routing.module.ts
+++ AppGG2/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddPersonaComponent } from './component/add-persona/add-persona.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "inserisci" , component:AddPersonaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AppGG2/src/app/app.module.ts
+++ AppGG2/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonaComponent } from './component/persona/persona.component';
 import { ListapersoneComponent } from './component/listapersone/listapersone.component';
+import { AddPersonaComponent } from './component/add-persona/add-persona.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonaComponent,
-    ListapersoneComponent
+    ListapersoneComponent,
+    AddPersonaComponent
    
   ],
   imports: [
